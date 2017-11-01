2b07258ac3fbb9d594e5b1f584852c9386d76543
OleKjaerulff <okjaerulff@sund.ku.dk>
2017-11-01 21:36:43 +0100

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AlarmBroadcastTest {
+
+    using Android.App;
+    using Android.Widget;
+    using Android.OS;
+    using Android.Content;
+
+    [Activity(Label = "AlarmBroadcastTest", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            SetAlarm();
+        }
+
+        public void SetAlarm()
+        {
+            Intent intent = new Intent(this, typeof(MyTestReceiver));
+            PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, intent, PendingIntentFlags.CancelCurrent);
+            AlarmManager alarmManager = (AlarmManager)this.GetSystemService(Context.AlarmService);
+            alarmManager.Set(AlarmType.ElapsedRealtime, SystemClock.ElapsedRealtime() +  5 * 1000, pendingIntent);
+        }
+    }
+
+    [BroadcastReceiver(Enabled = true)]
+    public class MyTestReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Toast toast = Toast.MakeText(Application.Context, "Hello", ToastLength.Long);
+            toast.Show();
+        }
+    }
+
+}
