934fbe47cd934f057a1b02f0d66e81b790a6d455
OleKjaerulff <okjaerulff@sund.ku.dk>
2017-11-01 21:43:13 +0100

    Annotated code

--- MainActivity.cs
+++ MainActivity.cs
@@ -19,23 +19,28 @@ namespace AlarmBroadcastTest {
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            //Calling function to rig alarm
             SetAlarm();
         }
 
         public void SetAlarm()
         {
+            //"this" is context, and "MyTestReceiver" the recipient of the intent - in total: name and address!)
             Intent intent = new Intent(this, typeof(MyTestReceiver));
             PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 0, intent, PendingIntentFlags.CancelCurrent);
             AlarmManager alarmManager = (AlarmManager)this.GetSystemService(Context.AlarmService);
+            //see http://sangsoonam.github.io/2017/03/01/do-not-use-curenttimemillis-for-time-interval.html
             alarmManager.Set(AlarmType.ElapsedRealtime, SystemClock.ElapsedRealtime() +  5 * 1000, pendingIntent);
         }
     }
 
     [BroadcastReceiver(Enabled = true)]
+    //no intent filter needed here
     public class MyTestReceiver : BroadcastReceiver
     {
         public override void OnReceive(Context context, Intent intent)
         {
+            //test message appearing 5 seconds after start
             Toast toast = Toast.MakeText(Application.Context, "Hello", ToastLength.Long);
             toast.Show();
         }
